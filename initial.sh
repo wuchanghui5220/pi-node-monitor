57f571503eacbd21f4c201badaad1222c528ad60
Karl <33740652+wuchanghui5220@users.noreply.github.com>
2021-12-10 12:27:43 +0800

    Update initial.sh

--- initial.sh
+++ initial.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+# 首次使用 winget 命令，需要同意 Windows源协议条款
 powershell.exe winget list --name "Docker"
+
+# winget 命令采集的信息文件需要转换为Linux 系统文件
+# 所以要安装 dos2unix 工具
 
 sudo apt update
 sudo apt install dos2unix
